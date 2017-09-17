01e104553813a55415494e621c7db1c21259e723
Leo Xia <leoxia360@gmail.com>
2017-09-17 16:33:51 -0500

    file redirection

--- program1/yash2.c
+++ program1/yash2.c
@@ -19,6 +19,7 @@ char *currentArg;
 char *previousArg;
 int foundFirstArg = 0;
 char firstArg[BUFFER_SIZE];
+char* args[BUFFER_SIZE];
 
 int main () {
 	while (1) {
@@ -27,12 +28,18 @@ int main () {
 		fgets(argv, BUFFER_SIZE, stdin);
 		
 		//argument parsing
+		int count = 0;
 		strtok(argv, "\n");
 		currentArg = strtok(argv, whitespace);
+		args[count] = currentArg;
+		//printf("%s\n", args[count]);
+		count += 1;
 		strcat(firstArg, currentArg);
 		while (currentArg != NULL) {
 			previousArg = currentArg;
 			currentArg = strtok(NULL, whitespace);
+			//args[count] = currentArg;
+			//printf("%s\n", args[count]);
 			//load file redirection locations
 			if (strcmp(previousArg, "<") == 0) {
 				inFile = currentArg;
@@ -49,14 +56,19 @@ int main () {
 					&& (strcmp(currentArg, "<") == 0
 						|| strcmp(currentArg, ">") == 0
 						|| strcmp(currentArg, "2>") == 0)) {
-					printf("firstArg: %s\n", firstArg);
+					//printf("firstArg: %s\n", firstArg);
 					foundFirstArg = 1;
 				} else if (foundFirstArg == 0) {
-					strcat(firstArg, " ");
-					strcat(firstArg, currentArg);
+					//strcat(firstArg, " ");
+					//strcat(firstArg, currentArg);
+					args[count] = currentArg;
+//					printf("%s\n", args[count]);
+					count += 1;
 				}
 			}
 		}
+		args[count] = NULL;
+//		printf("%s\n", args[count]);
 
 		//replace spaces in argv with null terminating chars
 		int i = 0;
@@ -66,16 +78,45 @@ int main () {
         	}
         	i++;
 		}
-	/*	
+//		printf("argv: %s\n", argv);
+		
+//		char currentDir[3] = "./";
+//		strcat(currentDir, inFile);
+//		printf("inFile: %s\n", inFile);
+//		printf("outFile: %s\n", outFile);
+//		printf("errFile: %s\n", errFile);
+	
 		//fork for file redirection
 		pid_redirect = fork();
 		if (pid_redirect == 0) {
 			//child code
-						
+			if (inFile != NULL) {
+				int in = open(inFile, O_RDONLY | O_CREAT, 0666);
+				dup2(in, STDIN_FILENO);
+				close(in);
+			}
+			if (outFile != NULL) {
+				int out = open(outFile, O_WRONLY | O_CREAT, 0666);
+				dup2(out, STDOUT_FILENO);
+				close(out);
+			}
+			if (errFile != NULL) {
+				int err = open(errFile, O_WRONLY | O_CREAT, 0666);
+				dup2(err, STDERR_FILENO);
+				close(err);
+			}				
+			execvp(args[0], args);					
+    		exit(0);
+		} else if (pid_redirect == -1) {
+			perror("waitpid");
+	  		exit(EXIT_FAILURE);
 		}
-*/
+		
 		// reset args, get ready for next user input
 		strcpy(firstArg, "");
 		foundFirstArg = 0;
+		inFile = NULL;
+		outFile = NULL;
+		errFile = NULL;
 	}
 }
